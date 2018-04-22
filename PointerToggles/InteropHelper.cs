b3b3d60cafba60e18620e151859c0e7f20ee9fa1
Jos635 <Jos635@outlook.com>
2018-04-22 16:11:51 +0200

    Initial commit

---
+++ PointerToggles/InteropHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointerToggles
+{
+    [Flags]
+    public enum SPIF
+    {
+        None = 0x00,
+        /// <summary>Writes the new system-wide parameter setting to the user profile.</summary>
+        SPIF_UPDATEINIFILE = 0x01,
+        /// <summary>Broadcasts the WM_SETTINGCHANGE message after updating the user profile.</summary>
+        SPIF_SENDCHANGE = 0x02,
+        /// <summary>Same as SPIF_SENDCHANGE.</summary>
+        SPIF_SENDWININICHANGE = 0x02
+    }
+
+    public static class InteropHelper
+    {
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo", SetLastError = true)]
+        public static extern bool SystemParametersInfoGet(uint action, uint param, IntPtr vparam, SPIF fWinIni);
+        public const UInt32 SPI_GETMOUSE = 0x0003;
+
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo", SetLastError = true)]
+        public static extern bool SystemParametersInfoSet(uint action, uint param, IntPtr vparam, SPIF fWinIni);
+        public const UInt32 SPI_SETMOUSE = 0x0004;
+    }
+}
